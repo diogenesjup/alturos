ae5316b386fb9a4394256c75b3c953263a052f4a
Diogenes Junior <diogenesjunior.ti@gmail.com>
2025-04-01 17:38:44 -0300

    correção repositório

--- www/views/Views.js
+++ www/views/Views.js
@@ -2129,7 +2129,7 @@ class Views{
 
                      <div class="form-group link-apoio text-center">
                             <a href="javascript:void(0)" title="Versão do Aplicativo" style="padding-top:20px;font-size:13px;">
-                               Versão 1.2.0
+                               Versão 1.0.0
                             </a>
                           </div>
                      
